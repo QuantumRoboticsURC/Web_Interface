ccb1a3de31f2a4e0cb519e1ad95bb25292eac188
AriadnaHCTec <A01749161@tec.mx>
2022-06-01 22:56:32 -0500

    DDM added to coor conversor

--- static/js/coordinates.js
+++ static/js/coordinates.js
@@ -1,24 +1,51 @@
 function dmsversdd(){
     var lat = document.getElementById("lat").value;
-    var lat_degres = parseFloat(document.getElementById('latitude_degres').value) || 0;
+    var lat_degrees = parseFloat(document.getElementById('latitude_degrees').value) || 0;
     var lat_minutes = parseFloat(document.getElementById('latitude_minutes').value) || 0;
-    var lat_secondes = parseFloat(document.getElementById('latitude_secondes').value) || 0;
+    var lat_seconds = parseFloat(document.getElementById('latitude_seconds').value) || 0;
 
     if(lat === "nord"){
-        var lat_geo = (lat_degres + (lat_minutes / 60) + (lat_secondes / 3600)) 
+        var lat_geo = (lat_degrees + (lat_minutes / 60) + (lat_seconds / 3600)) 
     } else {
-        var lat_geo = -(lat_degres + (lat_minutes / 60) + (lat_secondes / 3600))
+        var lat_geo = -(lat_degrees + (lat_minutes / 60) + (lat_seconds / 3600))
     }
 
     var lon = document.getElementById("lon").value;
-    var lon_degres = parseFloat(document.getElementById('longitude_degres').value) || 0;
+    var lon_degrees = parseFloat(document.getElementById('longitude_degrees').value) || 0;
     var lon_minutes = parseFloat(document.getElementById('longitude_minutes').value) || 0 ;
-    var lon_secondes = parseFloat(document.getElementById('longitude_secondes').value) || 0;
+    var lon_seconds = parseFloat(document.getElementById('longitude_seconds').value) || 0;
 
     if(lon === "est"){
-        var lon_geo = (lon_degres + (lon_minutes / 60) + (lon_secondes / 3600)) 
+        var lon_geo = (lon_degrees + (lon_minutes / 60) + (lon_seconds / 3600)) 
     } else {
-        var lon_geo = -(lon_degres + (lon_minutes / 60) + (lon_secondes / 3600))
+        var lon_geo = -(lon_degrees + (lon_minutes / 60) + (lon_seconds / 3600))
+    }
+    document.getElementById('label_lat').innerText = lat_geo;
+    document.getElementById('label_lon').innerText = lon_geo;
+}
+
+function ddmversdd(){
+    var lat = document.getElementById("lat_ddm").value;
+    var lat_degrees = parseFloat(document.getElementById('latitude_degrees_ddm').value) || 0;
+    var lat_minutes = parseFloat(document.getElementById('latitude_minutes_ddm').value) || 0;
+    var lat_seconds = (lat_minutes % 1) * 60;
+    lat_minutes = Math.floor(lat_minutes);
+
+    if(lat === "nord"){
+        var lat_geo = (lat_degrees + (lat_minutes / 60) + (lat_seconds / 3600)) 
+    } else {
+        var lat_geo = -(lat_degrees + (lat_minutes / 60) + (lat_seconds / 3600))
+    }
+
+    var lon = document.getElementById("lon_ddm").value;
+    var lon_degrees = parseFloat(document.getElementById('longitude_degrees_ddm').value) || 0;
+    var lon_minutes = parseFloat(document.getElementById('longitude_minutes_ddm').value) || 0 ;
+    var lon_seconds = (lon_minutes % 1) * 60;
+    lon_minutes = Math.floor(lon_minutes);
+    if(lon === "est"){
+        var lon_geo = (lon_degrees + (lon_minutes / 60) + (lon_seconds / 3600)) 
+    } else {
+        var lon_geo = -(lon_degrees + (lon_minutes / 60) + (lon_seconds / 3600))
     }
     document.getElementById('label_lat').innerText = lat_geo;
     document.getElementById('label_lon').innerText = lon_geo;
